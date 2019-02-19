69920e6e6a3e46da8b1484afa43802d1df061e37
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2019-02-19 13:46:29 -0600

    Setup basic map view & location data

--- Tourio/ViewControllers/TourViewController.swift
+++ Tourio/ViewControllers/TourViewController.swift
@@ -7,13 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class TourViewController: UIViewController {
+class TourViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Loaded tour view")
+        locationSetup()
+        mapSetup()
+        addPoints()
+    }
+    
+    func locationSetup() {
+        locManager = CLLocationManager()
+        locManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locManager.delegate = self
+        locManager.requestWhenInUseAuthorization()
+        locManager.startUpdatingLocation()
+    }
+    
+    func mapSetup() {
+        // setup map appearance
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+        
+        // how far the map spans
+        let viewSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        // center the map on the current location
+        if let currentPos = locManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: currentPos, span: viewSpan)
+            mapView.setRegion(viewRegion, animated: true)
+        }
+    }
+    
+    // add the annotations for the current tour
+    func addPoints() {
+        // TODO: if on tour...
+        // build a test point
+        let testCoord = CLLocationCoordinate2D(latitude: 33.1348324, longitude: -96.7718147)
+        let testPoint = MapPointView(title: "Test point", subtitle: "this is a test!", coordinate: testCoord)
+        
+        // add it to the map
+        mapView.addAnnotation(testPoint)
+    }
+    
+    // handle errors from the location manager
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    // get the last location from the manager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations[locations.count - 1]
+        print(lastLocation.coordinate)
     }
 
 }
