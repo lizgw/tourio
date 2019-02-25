5e8b29d47ff0c7ff3df811273bb1e406a16629aa
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2019-02-25 14:05:23 -0600

    Build out basic profile view

--- Tourio/ViewControllers/ProfileViewController.swift
+++ Tourio/ViewControllers/ProfileViewController.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, TourListingViewProtocol {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var myToursStackView: UIStackView!
+    
+    var username = "user"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("Loaded profile view")
+        
+        // for demo, actual username from network
+        username = "user\(Int.random(in: 1...100))"
+        
+        displayInfo()
+    }
+    
+    func displayInfo() {
+        // display the username
+        usernameLabel.text = username
+        
+        // get the profile image
+        profileImageView.image = UIImage(named: "UserIcon", in: nil, compatibleWith: nil)
+        
+        // create a few random tours to show in the profile
+        for tourNum in 1...4 {
+            // make a random tour
+            let tour = Tour(createdBy: username, isOrdered: true)
+            tour.name = "Tour \(tourNum)\(Int.random(in: 1...100))"
+            tour.desc = "This is a basic tour description"
+            tour.iconPath = "UserIcon"
+            
+            // add the TourListingView to the stack view
+            let tourView = tour.getTourListingView()
+            tourView.delegate = self
+            myToursStackView.addArrangedSubview(tourView)
+        }
+    }
+    
+    func tourListingViewTapped(tour: Tour) {
+        // go to the details screen when a listing view is tapped
+        performSegue(withIdentifier: "ProfileToDetailsSegue", sender: tour)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // if we're going to the details segue
+        if (segue.identifier == "ProfileToDetailsSegue") {
+            
+            // check type for view controller and the Tour
+            guard let detailsVC = segue.destination as? TourDetailsViewController,
+                let tourObj = sender as? Tour else { return }
+            // set the view controller's tour property
+            detailsVC.tour = tourObj
+        }
     }
     
 }
