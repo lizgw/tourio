3f165e9ccf4d1be86040ae4764003c208a8fce2d
Ryan Talbot <ryan.talbot.829@k12.friscoisd.org>
2019-04-30 14:22:41 -0500

    Finished name/description screen for Tour Creation, and added tour information to database upon creation.

--- Tourio/ViewControllers/CreateTourViewController.swift
+++ Tourio/ViewControllers/CreateTourViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateTourViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var descriptionField: UITextView!
     
     override func viewDidLoad() {
@@ -17,8 +19,44 @@ class CreateTourViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        nameField.layer.borderColor = UIColor.lightGray.cgColor
+        nameField.layer.borderWidth = 1
         descriptionField.layer.borderColor = UIColor.lightGray.cgColor
         descriptionField.layer.borderWidth = 1
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? TourDetailsViewController else {
+            return
+        }
+        
+        var tour = Tour(createdBy: "Ryan", isOrdered: false)
+        tour.name = nameField.text!
+        tour.desc = descriptionField.text
+        
+        let db = Firestore.firestore()
+        
+        var ref : DocumentReference? = nil
+        
+        ref = db.collection("testTours").document(tour.name)
+        
+        db.collection("cities").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        ref!.setData([
+            "Description": descriptionField.text,
+            "Creator": "Ryan"
+            ])
+        
+        ref?.collection("points").addDocument(data: ["Name": "Point A"])
+        
+        destination.tour = tour
     }
 
 }
